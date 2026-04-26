789922d49331a33c828e1cfe2d8ff883582f6187
Youssef Hesham William <youssefhesham1811@gmail.com>
2026-04-27 00:01:33 +0300

    Improve database connection and user data formatting
    Refactor database connection and enhance user data retrieval.

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ import os
 
 app = Flask(__name__)
 
-conn = psycopg2.connect(os.environ.get("DATABASE_URL"))
+# الاتصال بالداتابيز (Neon)
+conn = psycopg2.connect(
+    os.environ.get("DATABASE_URL"),
+    sslmode="require"
+)
 
 @app.route("/")
 def home():
@@ -14,5 +18,12 @@ def home():
 def users():
     cur = conn.cursor()
     cur.execute("SELECT * FROM users;")
-    data = cur.fetchall()
-    return jsonify(data)
+
+    columns = [desc[0] for desc in cur.description]
+    rows = cur.fetchall()
+
+    result = []
+    for row in rows:
+        result.append(dict(zip(columns, row)))
+
+    return jsonify(result)
