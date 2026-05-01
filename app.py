f123ab38bbaaa39d3f28ca13fefff99d90e4d195
Youssef Hesham William <youssefhesham1811@gmail.com>
2026-05-01 15:28:41 +0300

    Add new route to list all registered routes

--- app.py
+++ app.py
@@ -6,11 +6,20 @@ from routes.favorites import favorites_bp
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return "API is running"
 
-# تسجيل كل الـ routes
+
+@app.route("/routes")
+def routes():
+    return {
+        str(rule): sorted(rule.methods)
+        for rule in app.url_map.iter_rules()
+    }
+
+
 app.register_blueprint(products_bp)
 app.register_blueprint(users_bp)
 app.register_blueprint(cart_bp)
